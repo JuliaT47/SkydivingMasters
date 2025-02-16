29a09bc2b1f3f534eb7d6034f1e07b271531a6ad
JuliaT47 <Kawaiineko55@gmail.com>
2025-02-16 14:09:29 +0000

    Mobile menu

--- modal.js
+++ modal.js
@@ -1,8 +1,11 @@
 const modal = document.querySelector(".backdrop");
-const modalBtnOpen = document.querySelector(".modal-button-open");
+const modalBtnOpen = document.querySelectorAll(".modal-button-open");
+console.log("Modal open button:", modalBtnOpen);
 const modalBtnClose = document.querySelector(".modal-button-close");
 
 const toggleModal = () => modal.classList.toggle("is-hidden");
 
-modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnOpen.forEach((button) => {
+  button.addEventListener("click", toggleModal);
+});
 modalBtnClose.addEventListener("click", toggleModal);
